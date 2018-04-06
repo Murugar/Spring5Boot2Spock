c7e2c39b8144dd80afc9bbb9ec8cf63bab9cb22b
Davanon <davanon2014@gmail.com>
2018-04-06 14:40:41 +0530

    First

---
+++ src/main/java/com/iqmsoft/SpecialClass.java
@@ -0,0 +1,12 @@
+package com.iqmsoft;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpecialClass {
+
+    public String sayHello() {
+        return "hello";
+    }
+
+}
---
+++ src/test/groovy/com/iqmsoft/test/FooSpec.groovy
@@ -0,0 +1,23 @@
+package com.iqmsoft.test
+
+import com.iqmsoft.Application
+import com.iqmsoft.SpecialClass
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest(classes = Application.class)
+class FooSpec extends Specification {
+
+    @Autowired
+    private SpecialClass specialClass;
+
+    def "should be able to get spring context autowired feature working in the tests"() {
+        when:
+            String response = specialClass.sayHello()
+
+        then:
+            "hello" == response
+    }
+
+}
